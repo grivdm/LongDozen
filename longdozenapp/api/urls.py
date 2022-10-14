ab067587a95b0c376db8a293444749f2849bbe22
grivdm <grivdm94@gmail.com>
2022-10-14 00:03:36 -0400

    filtering

--- longdozenapp/api/urls.py
+++ longdozenapp/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework import routers
+from django.urls import include
+
+router = routers.SimpleRouter()
+
+
+router.register('place', viewset=views.PlaceViewSet)
 
 urlpatterns = [
-    path('', views.PlaceList.as_view()),
+    path('v1/', include(router.urls)),
 
 ]
--- longdozenapp/api/views.py
+++ longdozenapp/api/views.py
@@ -1,14 +1,31 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 
 from .serializers import PlaceSerializer
 from longdozenapp.models import Place
+#
+#
+# class PlaceList(generics.ListAPIView):
+#     queryset = Place.objects.all()
+#     serializer_class = PlaceSerializer
+#     filter_backends = (DjangoFilterBackend, SearchFilter)
+#     filterset_fields = ('id', )
+#     search_fields = ('name',)
 
 
-class PlaceList(generics.ListAPIView):
+class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
-    filterset_fields = ('id', )
-    search_fields = ('name',)
+    filterset_fields = ['name', ]
+    search_fields = ['name', 'category']
+
+    def get_queryset(self):
+        category_filter = self.request.query_params.get('category', None)
+        if category_filter is None:
+            return super().get_queryset()
+        queryset = Place.objects.all()
+        category_id = category_filter
+        if category_id.isnumeric():
+            return queryset.filter(category_id=category_id)
